73719cdebca23af6c907771fd5f5090c3a8fa846
mlacoste92 <maximelacoste92@gmail.com>
2017-05-13 12:41:26 -0400

    allow crossd omain

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+		});
+
 app.use('/', routeIndex);
 app.use('/', routeCampings);
 
